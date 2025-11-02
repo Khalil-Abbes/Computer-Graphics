fd0fcc6570cdfc34cdd50da4f57853ba01908e81
Muhammad Umar Javed <muja00003@stud.uni-saarland.de>
2025-11-02 18:21:56 +0100

    Implement BVH Integrator (1.4.1)

--- src/integrators/aov.cpp
+++ src/integrators/aov.cpp
@@ -3,12 +3,14 @@
 namespace lightwave {
 class AOVIntegrator : public SamplingIntegrator {
     std::string m_variable;
+    int m_scale;
 
 public:
     AOVIntegrator(const Properties &properties)
         : SamplingIntegrator(properties) {
 
         m_variable = properties.get<std::string>("variable");
+        m_scale    = properties.get<int>("scale", 1);
     }
 
     Color Li(const Ray &ray, Sampler &rng) override {
@@ -23,6 +25,13 @@ public:
 
             shading_normal = (shading_normal + Vector(1.f, 1.f, 1.f)) / 2;
             return Color(shading_normal);
+        } else if (m_variable == "bvh") {
+            const Intersection intersection = m_scene->intersect(ray, rng);
+            Color shading_normal;
+            shading_normal = Color(intersection.stats.bvhCounter,
+                                   intersection.stats.primCounter,
+                                   0);
+            return shading_normal / m_scale;
         }
     }
 
