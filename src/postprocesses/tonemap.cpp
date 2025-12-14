cca1451d61290229bd65cb8a9e5be0dfd7b8f254
moab00008 <moab00008@stud.uni-saarland.de>
2025-12-14 12:11:41 +0100

    4.2 done

--- src/postprocesses/tonemap.cpp
+++ src/postprocesses/tonemap.cpp
@@ -5,15 +5,20 @@ namespace lightwave {
 class Tonemap : public Postprocess {
 
 public:
-    Tonemap(const Properties &properties) : Postprocess(properties) {
-    }
+    Tonemap(const Properties &properties) : Postprocess(properties) {}
 
     void execute() override {
         m_output->initialize(m_input->resolution());
 
-        NOT_IMPLEMENTED
+        // Standard single-threaded loop
+        // m_output->bounds() returns a range that iterates over every pixel
+        // coordinate
+        for (const Point2i &pixel : m_output->bounds()) {
+            Color c              = m_input->get(pixel);
+            m_output->get(pixel) = c / (c + Color(1.0f));
+        }
 
-        Streaming stream{*m_output};
+        Streaming stream{ *m_output };
         stream.update();
         m_output->save();
     }
